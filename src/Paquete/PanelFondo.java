1ae79d666a3f3e50e50d5ee2532b5f7773a5aace
zzMAZzz <108913357+zzMAZzz@users.noreply.github.com>
2022-12-09 10:36:00 -0600

    Initial steps

---
+++ src/Paquete/PanelFondo.java
@@ -0,0 +1,10 @@
+package Paquete;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class PanelFondo extends JPanel{
+    Color Background = Color.gray;
+    int tamax, ta, can;
+    
+}
