a727f7cd2ce5a65f6c29e80703d1b58947833dff
zzMAZzz <108913357+zzMAZzz@users.noreply.github.com>
2022-12-09 11:30:44 -0600

    Playground Complete
    The background area/ or game canvas is completed.

--- src/Paquete/PanelFondo.java
+++ src/Paquete/PanelFondo.java
@@ -1,10 +1,33 @@
 package Paquete;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class PanelFondo extends JPanel{
     Color Background = Color.gray;
-    int tamax, ta, can;
+    int tamanomax, tamano, cantidad,res;
     
+    public PanelFondo(int tamanomax, int cantidad){
+        this.tamanomax = tamanomax;
+        this.cantidad = cantidad;
+        this.tamano = tamanomax/cantidad;
+        this.res = tamanomax%cantidad;
+    }
+    
+    @Override
+    public void paint(Graphics pintor){
+        super.paint(pintor);
+        pintor.setColor(Background);
+        
+        // Para el eje X e Y:
+        for (int i = 0; i < cantidad; i++){
+            for (int j = 0; j < cantidad; j++){
+                pintor.fillRect((res/2)+(i*tamano), (res/2)+(j*tamano) , tamano-1, tamano-1);
+            }
+        }
+        
+    }
 }
+
+
